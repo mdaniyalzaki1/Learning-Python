d69caf4a875c22cb113f96aca24257a6ef1d4f14
mdaniyalzaki1 <mdaniyalzakii@gmail.com>
2025-02-12 19:10:24 +0500

    More

---
+++ Problems/Loops/number_of_characters.py
@@ -0,0 +1,13 @@
+# Prompt the user to input a string
+write = input("Write...:")
+
+# Initialize a counter to store the number of characters in the input
+count = 0
+
+# Iterate through each character in the input string
+for char in write:
+    # Increment the counter by 1 for each character
+    count += 1
+
+# Print the total number of characters in the input
+print(count)
---
+++ Problems/Loops/sum_of_digits.py
@@ -0,0 +1,6 @@
+digits = input("Enter a Number: ")
+sum = 0
+for digit in digits:
+    sum += int(digit)
+
+print(f"The sum of digits is {sum}")
